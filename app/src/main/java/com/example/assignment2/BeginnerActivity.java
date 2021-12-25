15f1428c088d91c3e294450e0e1b247455ea6e1b
CHIN KEAT <68838825+chewchinkeat@users.noreply.github.com>
2021-12-26 02:39:11 +0800

    Complete Part3(no insert new pics)
    -Set number of exercise in activity_beginner.xml (start line 42, end line 1473)(copy and paste
    -need change id and picture asset(activity_beginner.xml)
    -behind 20 min waste of time(copy and paste)

--- app/src/main/java/com/example/assignment2/BeginnerActivity.java
+++ app/src/main/java/com/example/assignment2/BeginnerActivity.java
@@ -3,6 +3,7 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class BeginnerActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class BeginnerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beginner);
+    }
+
+    public void Imagebuttonclicked(View view) {
     }
 }
