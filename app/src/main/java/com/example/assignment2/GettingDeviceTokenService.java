581887485ca4603971ebbf701dd1e2f8018ba39a
Ng Chia Chen <75148342+chiachen7@users.noreply.github.com>
2021-12-28 16:20:20 +0800

    Complete Part 17
    Cloud messaging

---
+++ app/src/main/java/com/example/assignment2/GettingDeviceTokenService.java
@@ -0,0 +1,19 @@
+package com.example.assignment2;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class GettingDeviceTokenService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d("NEW_TOKEN",s);
+    }
+
+}
